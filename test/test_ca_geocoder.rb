e4d1af0944cc983b0966a2d582b6f366913c778d
Jason Hagglund <jasonh@onthecity.org>
2013-05-06 15:06:25 -0700

    Fixing canadian geocoding via CaGeocoder, updating tests, adding sensible stuff to Gemfile

--- test/test_ca_geocoder.rb
+++ test/test_ca_geocoder.rb
@@ -10,22 +10,23 @@ class CaGeocoderTest < BaseGeocoderTest #:nodoc: all
   EOF
 
   def setup
-    @ca_full_hash = {:street_address=>"2105 West 32nd Avenue",:city=>"Vancouver", :state=>"BC"}
+    @ca_full_hash = {:street_address=>"1160 West Georgia Street",:city=>"Vancouver", :state=>"BC", :zip=>"V6E3H7"}
     @ca_full_loc = Geokit::GeoLoc.new(@ca_full_hash)
   end
 
   def test_geocoder_with_geo_loc_with_account
     response = MockSuccess.new
     response.expects(:body).returns(CA_SUCCESS)
-    url = "http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml"
+    url = "http://geocoder.ca/?locate=1160+West+Georgia+Street+BC+V6E3H7&auth=SOMEKEYVALUE&geoit=xml"
     Geokit::Geocoders::CaGeocoder.expects(:call_geocoder_service).with(url).returns(response)
+    #puts Geokit::Geocoders::CaGeocoder.geocode(@ca_full_loc)
     verify(Geokit::Geocoders::CaGeocoder.geocode(@ca_full_loc))
   end
 
   def test_service_unavailable
     response = MockFailure.new
     #Net::HTTP.expects(:get_response).with(URI.parse("http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml")).returns(response)
-    url = "http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml"
+    url = "http://geocoder.ca/?locate=1160+West+Georgia+Street+BC+V6E3H7&auth=SOMEKEYVALUE&geoit=xml"
     Geokit::Geocoders::CaGeocoder.expects(:call_geocoder_service).with(url).returns(response)
     assert !Geokit::Geocoders::CaGeocoder.geocode(@ca_full_loc).success
   end
@@ -33,8 +34,10 @@ class CaGeocoderTest < BaseGeocoderTest #:nodoc: all
   private
 
   def verify(location)
+    assert_equal "1160 West Georgia Street", location.street_address
     assert_equal "BC", location.state
     assert_equal "Vancouver", location.city
+    assert_equal "V6E3H7", location.zip
     assert_equal "49.243086,-123.153684", location.ll
     assert !location.is_us?
   end
